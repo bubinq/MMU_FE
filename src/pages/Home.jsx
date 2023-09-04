91d2cd9fbbf18ac59b5230fd5c4bf2589d4497ea
bubinq <emilbumov@gmail.com>
2023-09-04 14:27:01 +0300

    feat: testing sending cookies on all requests

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Flex, Grid, Heading, Alert, AlertIcon } from "@chakra-ui/react";
 import { useLoaderData } from "react-router-dom";
 import { requestExecuter } from "../utils";
+import { REQUEST_NEW_VERIFICATION } from "../constants";
 import specialtyService from "../services/specialty";
 import SpecialtyList from "../components/Specialties/SpecialtyList";
 import useScrollToTop from "../hooks/useScrollToTop";
@@ -27,7 +28,7 @@ const Home = () => {
         variant="subtle"
       >
         <AlertIcon />
-        Request a new Token
+        {REQUEST_NEW_VERIFICATION}
       </Alert>
       <Heading variant={"main"}>Specialties</Heading>
       <Grid
